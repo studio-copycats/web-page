238358e0b447f2fababf5ec427987ee18b7e7b73
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2016-03-20 21:34:29 -0300

    Se agre el server again

---
+++ main.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var engines = require('consolidate');
+
+var app = express();
+
+var port = 3000;
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+["css", "font-awesome", "fonts", "img", "js"].forEach(function(it) {
+  app.use("/" + it, express.static(__dirname + '/' + it));
+})
+
+app.listen(port);
+console.log("Listering in port " + port);
